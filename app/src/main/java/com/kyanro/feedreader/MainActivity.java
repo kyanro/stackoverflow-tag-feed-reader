da66aa62870dc15dbe6e1b59fb9343776198d1e0
kyanro <gw.fnct+github@gmail.com>
2015-01-18 19:27:41 +0900

    クリックしたときにタグとして入力されている値を取りたかったけどうまくいかないのでパス

--- app/src/main/java/com/kyanro/feedreader/MainActivity.java
+++ app/src/main/java/com/kyanro/feedreader/MainActivity.java
@@ -5,36 +5,42 @@ import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.kyanro.feedreader.models.Feed;
 import com.kyanro.feedreader.models.Feed.Entry;
-import com.kyanro.feedreader.network.ApiService;
-import com.kyanro.feedreader.network.ApiService.StackoverflowService;
+import com.kyanro.feedreader.network.Stackoverflow;
+import com.kyanro.feedreader.network.Stackoverflow.StackoverflowService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import retrofit.RestAdapter;
-import retrofit.RestAdapter.LogLevel;
-import retrofit.android.AndroidLog;
-import retrofit.converter.SimpleXMLConverter;
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
+import rx.android.view.OnClickEvent;
+import rx.android.view.ViewObservable;
+import rx.functions.Func1;
+import rx.functions.Func2;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    @InjectView(R.id.tag_etext)
+    EditText mTagEditText;
+    @InjectView((R.id.refresh_button))
+    View mRefreshButton;
     @InjectView(R.id.feed_listview)
     ListView mFeedListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,26 +50,129 @@ public class MainActivity extends ActionBarActivity {
 
         final List<Entry> entries = new ArrayList<>();
 
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(ApiService.STACKOVERFLOW_HOST)
-                .setConverter(new SimpleXMLConverter())
-                .setLog(new AndroidLog("myrx"))
-                .setLogLevel(LogLevel.FULL)
-                .build();
-
-        StackoverflowService service = restAdapter.create(StackoverflowService.class);
+        StackoverflowService service = Stackoverflow.getStackoverflowService();
 
         final feedAdapter feedAdapter = new feedAdapter(this, android.R.layout.simple_list_item_2, entries);
         mFeedListView.setAdapter(feedAdapter);
 
-        service.newest()
-                .flatMap(feed -> Observable.from(feed.entries))
-                .take(5)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(entry -> {
-                    entries.add(entry);
-                    feedAdapter.notifyDataSetChanged();
-                }, e -> Log.d("myrx", "error:" + e.getMessage()));
+
+        Observable<String> tagTextChangedStream =
+                Observable.<String>create(subscriber -> mTagEditText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        //Log.d("myrx", "s?:" + s);
+                        if (s == null) {
+                            subscriber.onNext(null);
+                        } else {
+                            subscriber.onNext(s.toString());
+                        }
+                    }
+                }))
+                        .startWith((String) null);
+
+        Observable<OnClickEvent> refreshClickStream = ViewObservable.clicks(mRefreshButton);
+
+        Observable<String> tagTextChangedCounted = tagTextChangedStream.publish().refCount();
+
+
+        Observable<OnClickEvent> clickCounted = refreshClickStream.publish().refCount();
+
+        refreshClickStream.join(tagTextChangedStream,
+                new Func1<OnClickEvent, Observable<OnClickEvent>>() {
+                    @Override
+                    public Observable<OnClickEvent> call(OnClickEvent onClickEvent) {
+                        Log.d("myrx", "left duration:");
+                        return clickCounted;
+                    }
+                },
+                new Func1<String, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(String s) {
+                        Log.d("myrx", "right duration:" + s);
+                        return tagTextChangedCounted;
+                    }
+                },
+                new Func2<OnClickEvent, String, String>() {
+                    @Override
+                    public String call(OnClickEvent onClickEvent, String s) {
+                        Log.d("myrx", "result:" + s);
+
+                        return s;
+                    }
+                })
+                .subscribe(s -> Log.d("myrx", "success:" + s));
+
+
+        //tagTextChangedStream,
+        //new Func1<OnClickEvent, Observable<OnClickEvent>>() {
+        //    @Override
+        //    public Observable<OnClickEvent> call(OnClickEvent onClickEvent) {
+        //        Log.d("myrx", "left selector:");
+        //        return Observable.empty();
+        //    }
+        //},
+        //new Func1<String, Observable<String>>() {
+        //    @Override
+        //    public Observable<String> call(String s) {
+        //        Log.d("myrx", "right selector:" + s);
+        //        return Observable.just(s);
+        //    }
+        //},
+        //new Func2<OnClickEvent, String, String>() {
+        //    @Override
+        //    public String call(OnClickEvent onClickEvent, String s) {
+        //        Log.d("myrx", "result:" + s);
+        //        return s;
+        //    }
+        //})
+        //.subscribe(s -> Log.d("myrx", "success:" + s));
+
+        //tagTextChangedStream.join(refreshClickStream,
+        //        new Func1<String, Observable<String>>() {
+        //            @Override
+        //            public Observable<String> call(String s) {
+        //                Log.d("myrx", "left selector:" + s);
+        //                return Observable.just(s);
+        //            }
+        //        },
+        //        new Func1<OnClickEvent, Observable<OnClickEvent>>() {
+        //            @Override
+        //            public Observable<OnClickEvent> call(OnClickEvent onClickEvent) {
+        //                Log.d("myrx", "right selector:");
+        //                return Observable.just(onClickEvent);
+        //            }
+        //        },
+        //        new Func2<String, OnClickEvent, String>() {
+        //            @Override
+        //            public String call(String s, OnClickEvent onClickEvent) {
+        //                Log.d("myrx", "result stream:" + s);
+        //                return s;
+        //            }
+        //        })
+        //        //.zipWith(refreshClickStream, (s, onClickEvent) -> s)
+        //        .subscribe(s -> Log.d("myrx", "success:" + s));
+        //
+        //Observable<Entry> entryStream =  refreshClickStream
+        //        //.zipWith(tagTextChangedStream.last(), (onClickEvent, s) -> s)
+        //        .flatMap(onClickEvent -> Observable.<String>just(null))
+        //        .flatMap(service::getFeedsTag)
+        //        .flatMap(feed -> Observable.from(feed.entries));
+        //
+        //entryStream
+        //        .take(5)
+        //        .observeOn(AndroidSchedulers.mainThread())
+        //        .subscribe(entry -> {
+        //            entries.add(entry);
+        //            feedAdapter.notifyDataSetChanged();
+        //        }, e -> Log.d("myrx", "error:" + e.getMessage()));
     }
 
     public static class feedAdapter extends ArrayAdapter<Entry> {
---
+++ app/src/main/java/com/kyanro/feedreader/network/Stackoverflow.java
@@ -0,0 +1,39 @@
+package com.kyanro.feedreader.network;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.kyanro.feedreader.models.Feed;
+
+import retrofit.RestAdapter.Builder;
+import retrofit.converter.SimpleXMLConverter;
+import retrofit.http.GET;
+import retrofit.http.Query;
+import rx.Observable;
+
+public class Stackoverflow {
+    public final static String STACKOVERFLOW_HOST = "http://stackoverflow.com/";
+
+    public static StackoverflowService stackoverflowService;
+
+    private Stackoverflow() {
+    }
+
+    public static synchronized StackoverflowService getStackoverflowService(){
+        if (stackoverflowService == null) {
+            stackoverflowService = new Builder()
+                    .setEndpoint(STACKOVERFLOW_HOST)
+                    .setConverter(new SimpleXMLConverter())
+                    .build()
+                    .create(StackoverflowService.class);
+        }
+        return stackoverflowService;
+    }
+
+    public interface StackoverflowService{
+        @GET("/feeds/tag?tagnames=java&sort=newest")
+        public Observable<Feed> getFeedsTag(@Query("tagnames") String tagnames);
+    }
+
+
+}
